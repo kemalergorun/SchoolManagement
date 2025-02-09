458b1143e50d00ec91fa5b14a77319fb0abaa226
kemalergorun <kemalergorun@gmail.com>
2025-02-09 23:19:36 +0300

    09022025s

--- src/main/java/com/techproed/schoolmanagementbackendb326/service/user/UserService.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/service/user/UserService.java
@@ -3,14 +3,27 @@ package com.techproed.schoolmanagementbackendb326.service.user;
 import com.techproed.schoolmanagementbackendb326.payload.request.user.UserRequest;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
 import com.techproed.schoolmanagementbackendb326.payload.response.user.UserResponse;
+import com.techproed.schoolmanagementbackendb326.service.validator.UniquePropertyValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
-
+@RequiredArgsConstructor
 @Service
 public class UserService {
+    private final UniquePropertyValidator uniquePropertyValidator;
     public ResponseMessage<UserResponse> saveUser(@Valid UserRequest userRequest, String userRole) {
         //validate unique prop.
+        uniquePropertyValidator.checkDuplication(
+                userRequest.getUsername(),
+                userRequest.getSsn(),
+                userRequest.getPhoneNumber(),
+                userRequest.getEmail()
+        );
+        //dto entity mapping
+
+
+
 
     }
 }
